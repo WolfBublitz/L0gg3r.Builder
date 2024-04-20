018a843dae81e309670b74af0f4aad5781b42428
Wolf Bublitz <wolf.bublitz@gmail.com>
2024-04-20 22:31:09 +0200

    test projects do now have access to internals

---
+++ src/Assembly.cs
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------------
+// <copyright file="Assembly.cs" company="L0gg3r">
+// Copyright (c) L0gg3r Project
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("LoggerBuilderTests")]
+[assembly: InternalsVisibleTo("LogSinkBuilderTests")]
